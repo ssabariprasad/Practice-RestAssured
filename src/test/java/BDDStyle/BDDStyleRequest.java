be95d82d08ef1de56dc2b97df6e6bc6b23e13e26
ssabariprasad <ssabariprasad@gmail.com>
2025-04-27 23:09:26 +0530

    second commit for BDD and Non BDD style tutorial

---
+++ src/test/java/BDDStyle/BDDStyleRequest.java
@@ -0,0 +1,62 @@
+package BDDStyle;
+
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class BDDStyleRequest {
+	
+	@Test
+	public void getMethod()
+	{ 
+		given()
+		  .header("x-api-key","reqres-free-v1")
+		   .baseUri("https://reqres.in")
+		.when()
+		   .get("/api/users")
+		   .prettyPrint();
+		
+	}
+
+	@Test
+	public void postMethod()
+	{   given()
+		  .baseUri("https://reqres.in")
+		  .header("x-api-key","reqres-free-v1")
+		  .contentType("application/json")
+		  .body("{\r\n"
+				+ "    \"name\": \"morpheus\",\r\n"
+				+ "    \"job\": \"leader\"\r\n"
+				+ "}")
+		.when()
+		  .post("/api/users").prettyPrint();
+	}
+	
+	@Test
+	public void putMethod()
+	{   given()
+		  .baseUri("https://reqres.in")
+		  .header("x-api-key","reqres-free-v1")
+		  .contentType("application/json")
+		  .body("{\r\n"
+		  		+ "    \"name\": \"Kingsley\",\r\n"
+		  		+ "    \"job\": \"QA Lead\"\r\n"
+		  		+ "}")
+		.when()
+		  .put("/api/users/2").prettyPrint();
+		
+	}
+	
+	@Test
+	public void deleteMethod()
+	{   given()
+		  .header("x-api-key","reqres-free-v1")
+		   .baseUri("https://reqres.in")
+		.when()
+		   .delete("/api/users/2")
+		   .prettyPrint();
+		
+	}
+	
+
+}
--- src/test/java/NonBDDStyle/NonBddStyleRequest.java
+++ src/test/java/NonBDDStyle/NonBddStyleRequest.java
@@ -1,6 +1,8 @@
 package NonBDDStyle;
 
-import org.testng.annotations.Test;
+
+
+import org.junit.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
@@ -11,12 +13,46 @@ public class NonBddStyleRequest {
 	@Test
 	public void getMethod()
 	{   RestAssured.baseURI="https://reqres.in";
-		RequestSpecification resSpec= RestAssured.given();
+		RequestSpecification resSpec= RestAssured.given().header("x-api-key","reqres-free-v1");
 		
-		Response response=resSpec.request(Method.GET,"/api/users/2");
+		Response response=resSpec.request(Method.GET,"/api/users");
 		System.out.println(response.asPrettyString());
 		
 	}
 
-
+	@Test
+	public void postMethod()
+	{   RestAssured.baseURI="https://reqres.in";
+		RequestSpecification resSpec= RestAssured.given().header("x-api-key","reqres-free-v1").contentType("application/json").body("{\r\n"
+				+ "    \"name\": \"morpheus\",\r\n"
+				+ "    \"job\": \"leader\"\r\n"
+				+ "}");
+		
+		Response response=resSpec.request(Method.POST,"/api/users");
+		System.out.println(response.asPrettyString());
+		
+	}
+	
+	@Test
+	public void putMethod()
+	{   RestAssured.baseURI="https://reqres.in";
+		RequestSpecification resSpec= RestAssured.given().header("x-api-key","reqres-free-v1").contentType("application/json").body("{\r\n"
+				+ "    \"name\": \"Kingsly\",\r\n"
+				+ "    \"job\": \"Anand\"\r\n"
+				+ "}");
+		
+		Response response=resSpec.request(Method.PUT,"/api/users/2");
+		System.out.println(response.asPrettyString());
+		
+	}
+	
+	@Test
+	public void deleteMethod()
+	{   RestAssured.baseURI="https://reqres.in";
+		RequestSpecification resSpec= RestAssured.given().header("x-api-key","reqres-free-v1");
+		
+		Response response=resSpec.request(Method.DELETE,"/api/users/2");
+		System.out.println(response.asPrettyString());
+		
+	}
 }
